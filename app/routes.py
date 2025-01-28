24bfefa64b9bf727294cb66fa6688fe7964f97d5
Saleck07 <salekamein@gmail.com>
2025-01-28 09:43:56 +0000

    feat-2 update

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/<name>')
+def sayHello(name):
+    return f'Hello {name}'
